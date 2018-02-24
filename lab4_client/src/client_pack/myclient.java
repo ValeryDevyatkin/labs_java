8a25578d6767ef0c0410152e233c855a53f5e18e
Valera Deviatkin <valera9kin@inbox.ru>
2018-02-25 00:46:41 +0300

    fixed_exit

--- lab4_client/src/client_pack/myclient.java
+++ lab4_client/src/client_pack/myclient.java
@@ -8,6 +8,8 @@ import java.awt.GridBagLayout;
 import java.awt.TextArea;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.util.Iterator;
 import java.util.List;
 import javax.swing.JComboBox;
@@ -87,6 +89,11 @@ public class myclient extends JFrame implements ActionListener {
         add(bbook, c);
         
         show();
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
     }
     
     public static void main(String []args) {
--- lab5_EJB_CLIENT/src/java/lab5_ejb_client/Main.java
+++ lab5_EJB_CLIENT/src/java/lab5_ejb_client/Main.java
@@ -9,6 +9,8 @@ import java.awt.GridBagLayout;
 import java.awt.TextArea;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import javax.ejb.EJB;
 import javax.swing.JFrame;
 
@@ -119,6 +121,11 @@ public class Main extends JFrame implements ActionListener{
         add(badd, c);
         
         show();
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
     }
     
     public static void main(String[] args) {
